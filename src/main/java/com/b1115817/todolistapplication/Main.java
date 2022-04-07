b0718640318930be3af704a108bdd1501f3abee8
james <james@LAPTOP-6UUIJUOH.home>
2022-04-07 22:18:38 +0100

    Set up the JFrame

--- src/main/java/com/b1115817/todolistapplication/Main.java
+++ src/main/java/com/b1115817/todolistapplication/Main.java
@@ -1,11 +1,17 @@
 package com.b1115817.todolistapplication;
 
+import javax.swing.*;
 /**
  *
  * @author james
  */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Second time using Git");
+public class Main extends JFrame{
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        
+        frame.setTitle("To-do list");
+        frame.setSize(900,600);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
