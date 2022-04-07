bf878aef4dcd6b74d0303a8ba0ca80a6db4d92a1
james <james@LAPTOP-6UUIJUOH.home>
2022-04-07 22:07:15 +0100

    Initial commit

---
+++ src/main/java/com/b1115817/todolistapplication/Main.java
@@ -0,0 +1,11 @@
+package com.b1115817.todolistapplication;
+
+/**
+ *
+ * @author james
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("First time using Git");
+    }
+}
